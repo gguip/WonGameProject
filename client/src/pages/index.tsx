a7190178c618764cd1055e1a8d186302880a1793
Guilherme <guilhermepassarinho@gmail.com>
2021-12-05 19:48:43 -0300

    test husky

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -6,7 +6,7 @@ type Props = {
   title: string
 }
 
-export default function Home({ title = 'React Teste' }: Props) {
+export default function Home  ({ title = 'React Teste' }: Props) {
   return (
     <div className={styles.container}>
       <Head>
